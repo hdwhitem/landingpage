add5cbafaeca8b4663240878ae68e08e372e7e7b
hdwhitem <hdwhite.m@gmail.com>
2022-11-14 20:11:34 +0000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+
+function App() {
+
+  return (
+    <div className='text-3xl'>
+      app
+    </div>
+  )
+}
+
+export default App
