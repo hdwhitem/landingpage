3c46c086f43c0eb09a2e11e8c9db594e7bd37189
hdwhitem <hdwhite.m@gmail.com>
2022-11-18 11:40:49 +0000

    Add folder constants, file index.jsx implemented to FeatureCard on Business.jsx

--- src/components/Business.jsx
+++ src/components/Business.jsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import { features } from "../constants"
+
+const FeatureCard = ({label, title, content, index}) => (
+    <div className={`flex flex-col space-y-3 ${index !== features.length - 1 ? "mb-6" : "mb-0"} md:space-y-0 md:space-x-6 md:flex-row`}>
+        <div className='rounded-full bg-brightRedSupLight md:bg-transparent'>
+            <div className='flex items-center space-x-2'>
+                <div className='px-4 py-2 text-white rounded-full md:py-1 bg-brightRed'>
+                    {label}
+                </div>
+                <h3 className='text-base font-bold md:mb-4 md:hidden'>
+                    {title}
+                </h3>
+            </div>
+        </div>  
+        <div>
+            <h3 className='hidden mb-4 text-lg font-bold md:block'>
+                {title}
+            </h3>
+            <p className='text-darkGrayishBlue'>
+                {content}
+            </p>
+        </div>
+            
+    </div>
+)
 
 const Business = () => {
   return (
-    <div className='container flex flex-col px-4 mx-auto mt-2 space-y-12 md:space-y-0 md:flex-row'>
+    <div className='container flex flex-col mx-auto mt-2 space-y-12 md:space-y-0 md:flex-row'>
         <div className='flex flex-col space-y-12 md:w-1/2'>
             <h2 className='max-w-md text-4xl font-bold text-center md:text-left'>
                 What's different about Manage?
@@ -11,29 +35,13 @@ const Business = () => {
                 Manage provides all the functionality your team needs, without the complexity. Our software is tailor-made for modern digital product teams.
             </p>
         </div>
-        <div className='flex flex-col space-x-8 md:w-1/2'>
-            <div className='flex flex-col space-y-3 md:space-y-0 md:space-x-6 md:flex-row'>
-                <div className='rounded-full bg-brightRedSupLight md:bg-transparent'>
-                    <div className='flex items-center space-x-2'>
-                        <div className='px-4 py-2 text-white rounded-full md:py-1 bg-brightRed'>
-                            01
-                        </div>
-                        <h3 className='text-base font-bold md:mb-4 md:hidden'>
-                            Track company-wide progress
-                        </h3>
-                    </div>
-                </div>  
-                <div>
-                    <h3 className='hidden mb-4 text-lg font-bold md:block'>
-                        Track company-wide progress
-                    </h3>
-                    <p className='text-darkGrayishBlue'>
-                        See how your day-to-day tasks fit into the wider vision. Go from tracking progress at the milestone level all the way done to the smallest of details. Never lose sight of the bigger picture again.
-                    </p>
-                </div>
-                    
-            </div>
-        </div>
+        
+        <div className="flex flex-col md:w-1/2">
+        {features.map((feature, index) => (
+          <FeatureCard key={feature.id} {...feature} index={index}/>
+        ))}
+      </div>
+      
     </div>
   )
 }
---
+++ src/constants/index.jsx
@@ -0,0 +1,23 @@
+export const features = [
+    {
+        id: "feature-1",
+        label: "01",
+        title: "Track company-wide progress",
+        content:
+        "See how your day-to-day tasks fit into the wider vision. Go from tracking progress at the milestone level all the way down to the smallest of details. Never lose sight of the bigger picture again.",
+    },
+    {
+        id: "feature-2",
+        label: "02",
+        title: "Advanced built-in reports",
+        content:
+          "Set internal delivery estimates and track progress toward company goals. Our customisable dashboard helps you build out the reports you need to keep key stakeholders informed.",
+    },
+    {
+        id: "feature-3",
+        label: "03",
+        title: "Everything you need in one place",
+        content:
+          "Stop jumping from one service to another to communicate, store files, track tasks and share documents. Manage offers an all-in-one team productivity solution.",
+    },
+  ];
